c5c70d0cbf057e7498194c40d91f083f6aa85f23
mikio <mikio@aidkk.co.jp>
2025-08-15 16:32:39 +0900

    toggle diagnotics hover message

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -4,6 +4,14 @@ return {
     "hrsh7th/cmp-nvim-lsp",
   },
   config = function()
+    local show_diagnostics_hover = true
+
+    local function toggle_diagnostics_hover()
+      show_diagnostics_hover = not show_diagnostics_hover
+      vim.notify("Diagnostics hover: " .. (show_diagnostics_hover and "ON" or "OFF"))
+    end
+    vim.keymap.set("n", "<leader>d", toggle_diagnostics_hover, { desc = "Toggle diagnostics hover" })
+
     vim.lsp.config("*", {
       on_attach = function(client, bufnr)
         local opts = { noremap = true, silent = true, buffer = bufnr }
@@ -16,14 +24,16 @@ return {
         vim.keymap.set("n", "g<", vim.diagnostic.goto_prev, opts)
         vim.api.nvim_create_autocmd("CursorHold", {
           callback = function()
-            vim.diagnostic.open_float(nil, {
-              focusable = false,
-              style = "minimal",
-              border = "single",
-              source = "always",
-              header = "",
-              prefix = "",
-            })
+            if show_diagnostics_hover then
+              vim.diagnostic.open_float(nil, {
+                focusable = false,
+                style = "minimal",
+                border = "single",
+                source = "always",
+                header = "",
+                prefix = "",
+              })
+            end
           end,
         })
         vim.opt.updatetime = 300
